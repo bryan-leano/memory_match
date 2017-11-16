025ed339f4428048178fb735bd2d616c5cfe4429
Bryan Leanoe <bryan.leano@gmail.com>
2017-11-16 13:05:52 -0700

    upload

---
+++ main.js
@@ -0,0 +1,21 @@
+//function flip(imgArray)
+
+
+
+$(document).ready( function() {
+    //$(".hidden").show()
+    $(".hidden").click(function(){
+        $("#1").show()
+    })
+        
+    
+                            //function(){
+        //var id = $(this).attr("id")
+        //flip(images)
+
+
+
+// var button = document.getElementbyID("1")
+//     button.addEventListener("click")
+//         ready()
+//     })
